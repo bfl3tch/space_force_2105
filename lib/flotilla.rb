2d22ffeef62e9b96951d9333f37e66768a8df643
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-03 12:57:40 -0400

    Create personnel by ship method to pass test

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -17,27 +17,43 @@ class Flotilla
   end
 
   def recommend_personnel(spacecraft)
-    recommended = []
+    @recommended = []
     experienced = []
 
     craft_keys = spacecraft.requirements.flat_map do |requirement|
       requirement.keys
     end
+
     craft_values = spacecraft.requirements.flat_map do |requirement|
       requirement.values
     end
+
     @personnel.find_all do |person|
       if person.experience > craft_values.max
         experienced << person
       end
     end
+
     experienced.find_all do |pers|
      craft_keys.each do |key|
         if pers.specialties.any?(key) == true
-          recommended << pers
+          @recommended << pers
         end
       end
     end
-    recommended.uniq
+    @recommended.uniq
+  end
+
+  def personnel_by_ship
+    ships_persons = {}
+    @ships.each do |ship|
+      if ships_persons[ship] == nil
+        ships_persons[ship] = recommend_personnel(ship)
+      else
+        ships_persons[ship] << recommend_personnel(ship)
+      end
+    end
+    require "pry"; binding.pry
+    ships_persons
   end
 end
