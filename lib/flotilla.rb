3b07a1237b94168916700404f5eee557a460f225
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-03 11:25:22 -0400

    Update flotilla class to add ships to fleet

---
+++ lib/flotilla.rb
@@ -0,0 +1,15 @@
+class Flotilla
+  attr_reader :name, :personnel, :ships
+  
+  def initialize(flotilla_info)
+    @name = flotilla_info[:designation]
+    @personnel = []
+    @ships = []
+
+  end
+
+  def add_ship(ship)
+    @ships << ship
+  end
+
+end
