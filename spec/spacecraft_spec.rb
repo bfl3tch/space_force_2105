bf099fb99ee030c0593ee19ff33b08737d64c135
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-03 11:28:56 -0400

    Create test for new attribute requirements

--- spec/spacecraft_spec.rb
+++ spec/spacecraft_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe do
       expect(daedalus.fuel).to eq(400)
     end
   end
+  describe 'its methods' do
+    it 'can add requirements to itself' do
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+      expect(daedalus.requirements).to eq([])
+      daedalus.add_requirement({astrophysics: 6})
+      daedalus.add_requirement({quantum_mechanics: 3})
+
+      expect(daedalus.requirements).to eq([{astrophysics: 6}, {quantum_mechanics: 3}])
+    end
+  end
 end
