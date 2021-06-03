849ce9f140592d1e52de8d6b075dbabee65d4443
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-03 11:24:53 -0400

    Create test for adding ships to flotilla

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -17,6 +17,15 @@ RSpec.describe do
   end
 
   describe 'its methods' do
+    it 'can add ships to its fleet' do
+    seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
 
+    expect(seventh_flotilla.ships).to eq([])
+
+    seventh_flotilla.add_ship(daedalus)
+
+    expect(seventh_flotilla.ships).to eq([daedalus])
+    end
   end
 end
