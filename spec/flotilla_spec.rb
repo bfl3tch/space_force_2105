cdd98f91ef5892ba0d957586001ac171729a5b5f
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-03 11:34:49 -0400

    Create test to add personnel to flotilla

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -27,5 +27,36 @@ RSpec.describe do
 
     expect(seventh_flotilla.ships).to eq([daedalus])
     end
+
+    it 'can add personnel' do
+      seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+      kathy = Person.new('Kathy Chan', 10)
+      kathy.add_specialty(:astrophysics)
+      kathy.add_specialty(:quantum_mechanics)
+
+      polly = Person.new('Polly Parker', 8)
+      polly.add_specialty(:operations)
+      polly.add_specialty(:maintenance)
+
+      rover = Person.new('Rover Henriette', 1)
+      rover.add_specialty(:operations)
+      rover.add_specialty(:maintenance)
+
+      sampson = Person.new('Sampson Edwards', 7)
+      sampson.add_specialty(:astrophysics)
+      sampson.add_specialty(:quantum_mechanics)
+
+      expect(seventh_flotilla.personnel).to eq([])
+
+      seventh_flotilla.add_personnel(kathy)
+      seventh_flotilla.add_personnel(polly)
+      seventh_flotilla.add_personnel(rover)
+      seventh_flotilla.add_personnel(sampson)
+
+      expect(seventh_flotilla.personnel).to eq([kathy, polly, rover, sampson])
+
+    end
   end
 end
